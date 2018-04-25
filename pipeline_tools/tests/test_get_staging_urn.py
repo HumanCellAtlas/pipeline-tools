f58cfd65b72012850002a98faba8002aa23ee449
Saman Ehsan <se8ea@virginia.edu>
2018-04-25 17:19:41 -0400

    Add retries to get_staging_urn

--- pipeline_tools/tests/test_get_staging_urn.py
+++ pipeline_tools/tests/test_get_staging_urn.py
@@ -1,19 +1,33 @@
 import unittest
+import requests
+import requests_mock
 import pipeline_tools.get_staging_urn as gsu
+from tenacity import stop_after_attempt, stop_after_delay, RetryError
+
 
 class TestGetStagingUrn(unittest.TestCase):
 
-    def test_empty_js(self):
+    def setUp(self):
+        self.envelope_url = 'http://api.ingest.integration.data.humancellatlas.org/submissionEnvelopes/abcde'
+        self.envelope_json = {
+            'stagingDetails': {
+                'stagingAreaLocation': {
+                    'value': 'test_urn'
+                }
+            }
+        }
+
+    def test_get_staging_urn_empty_js(self):
         js = {}
         self.assertIsNone(gsu.get_staging_urn(js))
 
-    def test_null_details(self):
+    def test_get_staging_urn_null_details(self):
         js = { 
             'stagingDetails': None
         }
         self.assertIsNone(gsu.get_staging_urn(js))
 
-    def test_null_location(self):
+    def test_get_staging_urn_null_location(self):
         js = { 
             'stagingDetails': {
                 'stagingAreaLocation': None
@@ -21,7 +35,7 @@ class TestGetStagingUrn(unittest.TestCase):
         }
         self.assertIsNone(gsu.get_staging_urn(js))
 
-    def test_null_value(self):
+    def test_get_staging_urn_null_value(self):
         js = { 
             'stagingDetails': {
                 'stagingAreaLocation': {
@@ -31,15 +45,40 @@ class TestGetStagingUrn(unittest.TestCase):
         }
         self.assertIsNone(gsu.get_staging_urn(js))
 
-    def test_valid_value(self):
-        js = { 
-            'stagingDetails': {
-                'stagingAreaLocation': {
-                    'value': 'test_urn'
-                }
-            }
-        }
-        self.assertEqual(gsu.get_staging_urn(js), 'test_urn')
+    def test_get_staging_urn_valid_value(self):
+        self.assertEqual(gsu.get_staging_urn(self.envelope_json), 'test_urn')
+
+    @requests_mock.mock()
+    def test_run(self, mock_request):
+        def _request_callback(request, context):
+            context.status_code = 200
+            return self.envelope_json
+        mock_request.get(self.envelope_url, json=_request_callback)
+        response = gsu.run(self.envelope_url)
+        self.assertEqual(mock_request.call_count, 1)
+
+    @requests_mock.mock()
+    def test_run_retry_if_urn_is_none(self, mock_request):
+        def _request_callback(request, context):
+            context.status_code = 200
+            return {}
+        mock_request.get(self.envelope_url, json=_request_callback)
+        with self.assertRaises(RetryError):
+            # Make the test complete faster by limiting the number of retries
+            response = gsu.run.retry_with(stop=stop_after_attempt(3))(self.envelope_url)
+        self.assertNotEqual(mock_request.call_count, 1)
+
+    @requests_mock.mock()
+    def test__run_retry_if_error(self, mock_request):
+        def _request_callback(request, context):
+            context.status_code = 500
+            return {}
+        mock_request.get(self.envelope_url, json=_request_callback)
+        with self.assertRaises(requests.HTTPError):
+            # Make the test complete faster by limiting the number of retries
+            response = gsu.run.retry_with(stop=stop_after_attempt(3))(self.envelope_url)
+        self.assertNotEqual(mock_request.call_count, 1)
+
 
 if __name__ == '__main__':
     unittest.main()
