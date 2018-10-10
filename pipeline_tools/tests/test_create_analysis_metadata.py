10f7ec4faae3e41e450bf3188a203e2718b479ca
Rex Wang <rexwangcc@gmail.com>
2018-10-10 17:26:50 -0400

    Update the file format output for zarr matrix. (#88)
    * Update the file format output for zarr matrix.
    
    * Address comments on the PR and improve regex.

--- pipeline_tools/tests/test_create_analysis_metadata.py
+++ pipeline_tools/tests/test_create_analysis_metadata.py
@@ -180,12 +180,14 @@ class TestCreateAnalysisMetadata(object):
 
     def test_get_file_format(self):
         assert cam.get_file_format('asdf', {}) == 'unknown'
-        assert cam.get_file_format('asdf.bam', {'.bam$': 'bam'}) == 'bam'
-        assert cam.get_file_format('asdf.txt', {'.bam$': 'bam'}) == 'unknown'
-        assert cam.get_file_format('asdf.bam', {'.bam$': 'bam', '_metrics': 'metrics'}) == 'bam'
-        assert cam.get_file_format('asdf.foo_metrics', {'.bam$': 'bam', '_metrics$': 'metrics'}) == 'metrics'
+        assert cam.get_file_format('asdf.bam', {'[.]bam$': 'bam'}) == 'bam'
+        assert cam.get_file_format('asdf.txt', {'[.]bam$': 'bam'}) == 'unknown'
+        assert cam.get_file_format('asdf.bam', {'[.]bam$': 'bam', '[_]metrics$': 'metrics'}) == 'bam'
+        assert cam.get_file_format('asdf.foo_metrics', {'[.]bam$': 'bam', '[_]metrics$': 'metrics'}) == 'metrics'
         assert cam.get_file_format('asdf.zarr!expression_matrix!id!.zarray',
-                                   {'.bam$': 'bam', '_metrics$': 'metrics', '.zarr!': 'zarr_format'}) == 'zarr_format'
+                                   {'[.]bam$': 'bam', '[_]metrics$': 'metrics', '[.]zattrs$': 'matrix'}) == 'unknown'
+        assert cam.get_file_format('asdf.zarr!.zattrs',
+                                   {'[.]bam$': 'bam', '[_]metrics$': 'metrics', '[.]zattrs$': 'matrix'}) == 'matrix'
 
     def test_get_outputs(self, test_data):
         schema_version = 'good_version'
