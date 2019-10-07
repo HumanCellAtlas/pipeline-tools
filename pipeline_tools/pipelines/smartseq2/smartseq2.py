d04150038bead39ab26c66b1bf77277c798d7f05
Saman Ehsan <se8ea@virginia.edu>
2019-10-07 15:54:11 -0400

    Update pipeline reference paths

--- pipeline_tools/pipelines/smartseq2/smartseq2.py
+++ pipeline_tools/pipelines/smartseq2/smartseq2.py
@@ -6,12 +6,12 @@ from pipeline_tools.shared.reference_id import ReferenceId
 REFERENCES = {
     ReferenceId.Human.value: {
         'hisat2_ref_trans_name': 'gencode_v27_trans_rsem',
-        'rrna_intervals': 'gs://hca-dcp-mint-test-data/reference/GRCh38_Gencode/gencode.v27.rRNA.interval_list',
-        'hisat2_ref_index': 'gs://hca-dcp-mint-test-data/reference/HISAT2/genome_snp_tran.tar.gz',
-        'genome_ref_fasta': 'gs://hca-dcp-mint-test-data/reference/GRCh38_Gencode/GRCh38.primary_assembly.genome.fa',
-        'hisat2_ref_trans_index': 'gs://hca-dcp-mint-test-data/reference/HISAT2/gencode_v27_trans_rsem.tar.gz',
-        'rsem_ref_index': 'gs://hca-dcp-mint-test-data/reference/GRCh38_Gencode/gencode_v27_primary.tar',
-        'gene_ref_flat': 'gs://hca-dcp-mint-test-data/reference/GRCh38_Gencode/GRCh38_gencode.v27.refFlat.txt',
+        'rrna_intervals': 'gs://hca-dcp-analysis-pipelines-reference/alignmentReferences/GRCh38_Gencode/gencode.v27.rRNA.interval_list',
+        'hisat2_ref_index': 'gs://hca-dcp-analysis-pipelines-reference/alignmentReferences/HISAT2/genome_snp_tran.tar.gz',
+        'genome_ref_fasta': 'gs://hca-dcp-analysis-pipelines-reference/alignmentReferences/GRCh38_Gencode/GRCh38.primary_assembly.genome.fa',
+        'hisat2_ref_trans_index': 'gs://hca-dcp-analysis-pipelines-reference/alignmentReferences/HISAT2/gencode_v27_trans_rsem.tar.gz',
+        'rsem_ref_index': 'gs://hca-dcp-analysis-pipelines-reference/alignmentReferences/GRCh38_Gencode/gencode_v27_primary.tar',
+        'gene_ref_flat': 'gs://hca-dcp-analysis-pipelines-reference/alignmentReferences/GRCh38_Gencode/GRCh38_gencode.v27.refFlat.txt',
         'hisat2_ref_name': 'genome_snp_tran',
         'stranded': 'NONE',
     }
