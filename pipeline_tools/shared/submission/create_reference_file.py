99fb3b48b97a4a3e89032e4aab114eb6768fc9fb
Gordon Wade <gwade@broadinstitute.org>
2020-08-15 11:48:35 -0400

    update version to get datetime

--- pipeline_tools/shared/submission/create_reference_file.py
+++ pipeline_tools/shared/submission/create_reference_file.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # import argparse
 import json
-import re
 import uuid
 
 
@@ -12,7 +11,7 @@ def build_file_descriptor(
     crc32c,
     raw_schema_url,
     file_descriptor_schema_version,
-    file_info,
+    version,
 ):
     """Create the submission envelope in Ingest service.
 
@@ -26,7 +25,6 @@ def build_file_descriptor(
 
     SCHEMA_TYPE = 'file_descriptor'
     entity_id = get_uuid5(sha256)
-    file_version = get_datetime_from_file_info(file_info)
 
     file_descriptor = {
         'describedBy': get_file_descriptor_described_by(
@@ -39,20 +37,12 @@ def build_file_descriptor(
         'sha256': sha256,
         'crc32c': crc32c,
         'file_id': entity_id,
-        'file_version': file_version,
+        'file_version': version,
         'file_name': get_relative_file_location(file_path),
     }
 
-    with open(f'{entity_id}_{file_version}.json', 'w') as f:
+    with open(f'{entity_id}_{version}.json', 'w') as f:
         json.dump(file_descriptor, f, indent=2, sort_keys=True)
-
-
-def get_datetime_from_file_info(file_info):
-    """Retrieve the datetime from the file info string and convert into mandated
-    format. Add '.000000' for microseconds"""
-    regex = r'([0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}Z)'
-    original_datetime = re.search(regex, file_info).group(0)
-    return original_datetime.replace('Z', '.000000Z')
 
 
 def get_uuid5(sha256):
