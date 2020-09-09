3ca0f51abc74fa167832d3000f4cd5630c09ab3e
Gordon Wade <gwade@broadinstitute.org>
2020-09-09 14:33:29 -0600

    format reference version properly

--- pipeline_tools/shared/submission/create_reference_file.py
+++ pipeline_tools/shared/submission/create_reference_file.py
@@ -3,7 +3,11 @@ import argparse
 import json
 import uuid
 
-from pipeline_tools.shared.submission.format_map import EXTENSION_TO_FORMAT, NAMESPACE
+from pipeline_tools.shared.submission.format_map import (
+    EXTENSION_TO_FORMAT,
+    NAMESPACE,
+    convert_datetime,
+)
 from pipeline_tools.shared.submission.create_analysis_metadata import get_file_format
 
 
@@ -31,6 +35,7 @@ def build_reference_file(
 
     SCHEMA_TYPE = 'file'
     entity_id = get_uuid5(get_uuid5(sha256))
+    formatted_version = convert_datetime(version)
 
     reference_file = {
         'describedBy': get_reference_file_described_by(
@@ -39,7 +44,7 @@ def build_reference_file(
         'schema_version': reference_file_schema_version,
         'schema_type': SCHEMA_TYPE,
         'file_core': get_file_core(file_path),
-        'provenance': {'document_id': entity_id, 'submission_date': version},
+        'provenance': {'document_id': entity_id, 'submission_date': formatted_version},
         'ncbi_taxon_id': int(ncbi_taxon_id),
         'genus_species': {'text': genus_species},
         'assembly_type': assembly_type,
@@ -48,7 +53,7 @@ def build_reference_file(
     }
 
     # Write the reference_file metadata
-    with open(f'{entity_id}_{version}.json', 'w') as f:
+    with open(f'{entity_id}_{formatted_version}.json', 'w') as f:
         json.dump(reference_file, f, indent=2, sort_keys=True)
 
     with open('reference_uuid.txt', 'w') as f:
