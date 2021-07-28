f694dbb743bf9ddbb2de47c560810df8ce767a47
Wes Dingman <wdingman@broadinstitute.org>
2021-07-28 12:23:37 -0400

    Wd refactor links (#184)
    * Links WIP
    
    * Links added with tests
    
    * add metadata.jsonfor reference

--- pipeline_tools/shared/schema_utils.py
+++ pipeline_tools/shared/schema_utils.py
@@ -8,5 +8,10 @@ SCHEMAS = {
         "describedBy" : "https://schema.humancellatlas.org/type/file/3.2.0/reference_file",
         "schema_type" : "file",
         "schema_version" : "3.2.0"
-    }
+    },
+    "LINKS" : {
+        "describedBy" : "https://schema.humancellatlas.org/system/2.1.1/links",
+        "schema_type" : "links",
+        "schema_version" : "2.1.1"
+    },
 }
