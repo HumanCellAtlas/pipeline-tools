82f64d3619c1514ca32904c6a9730661bc0aa736
cgold <cgold@asymmetrik.com>
2021-07-29 13:55:15 -0400

    pr comments

--- pipeline_tools/shared/submission/create_analysis_protocol.py
+++ pipeline_tools/shared/submission/create_analysis_protocol.py
@@ -2,7 +2,6 @@
 import argparse
 import json
 import os
-import uuid
 from pipeline_tools.shared.schema_utils import SCHEMAS
 from pipeline_tools.shared.submission import format_map
 
@@ -48,34 +47,26 @@ class AnalysisProtocol():
 
     def __init__(
         self,
-        input_uuid,
         method,
-        pipeline_version,
+        input_uuid,
         pipeline_type,
+        pipeline_version,
             workspace_version):
 
-        self.input_uuid = input_uuid
         self.computational_method = method
+        self.input_uuid = input_uuid
+        self.pipeline_type = pipeline_type
         self.protocol_core = {
             "protocol_id": pipeline_version
         }
-        self.pipeline_type = pipeline_type
-        self.provenance = {
-            "submission_date": workspace_version,
-            "update_date": workspace_version
-        }
-
-        string_to_hash = json.dumps(self.get_json())
-        entity_id = str(uuid.uuid5(format_map.NAMESPACE, string_to_hash)).lower()
-
-        self.provenance['document_id'] = entity_id
+        self.work_version = workspace_version
 
     def __analysis_protocol__(self):
         return {
             "computational_method": self.computational_method,
             "describedBy": self.describedBy,
             "protocol_core": self.protocol_core,
-            "provenance": self.provenance,
+            "provenance": self.__get_provenance(),
             "schema_type": self.schema_type,
             "type": self.type
         }
@@ -83,24 +74,51 @@ class AnalysisProtocol():
     def get_json(self):
         return self.__analysis_protocol__()
 
+    # Return json to hash for entity_id
+    def __get_hash_input(self):
+        return {
+            "computational_method": self.computational_method,
+            "describedBy": self.describedBy,
+            "protocol_core": self.protocol_core,
+            "schema_type": self.schema_type,
+            "type": self.type
+        }
+
+    # Generate provenance
+    def __get_provenance(self):
+        string_to_hash = json.dumps(self.__get_hash_input())
+        entity_id = format_map.get_uuid5(string_to_hash)
+
+        return {
+            "document_id": entity_id,
+            "submission_date": self.workspace_version,
+            "update_date": self.workspace_version
+        }
+
     @property
-    def uuid(self):
-        return self.input_uuid
+    def entity_id(self):
+        provenance = self.__get_provenance()
+        entity_id = provenance["document_id"]
+        return entity_id
+
+    @property
+    def workspace_version(self):
+        return self.work_version
 
 
 # Entry point for unit tests
 def test_build_analysis_protocol(
-    input_uuid,
     method,
-    pipeline_version,
+    input_uuid,
     pipeline_type,
+    pipeline_version,
         workspace_version):
 
     test_analysis_protocol = AnalysisProtocol(
-        input_uuid,
         method,
-        pipeline_version,
+        input_uuid,
         pipeline_type,
+        pipeline_version,
         workspace_version
     )
     return test_analysis_protocol.get_json()
@@ -108,46 +126,46 @@ def test_build_analysis_protocol(
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--input_uuid', required=True, help='Input file UUID from the HCA Data Browser')
+    parser.add_argument("--pipeline_type", required=True, help="Type of pipeline(SS2 or Optimus)")
+    parser.add_argument("--input_uuid", required=True, help="Input file UUID from the HCA Data Browser")
+    parser.add_argument("--workspace_version", required=True, help="Workspace version value i.e. timestamp for workspace")
     parser.add_argument(
-        '--method',
+        "--method",
         required=True,
-        help='Supposed to be method store url, for now can be url for wdl in skylab, or the name of'
-        ' the analysis workflow.',
+        help="Supposed to be method store url, for now can be url for wdl in skylab, or the name of"
+        " the analysis workflow.",
     )
     parser.add_argument(
-        '--pipeline_version',
+        "--pipeline_version",
         required=True,
-        help='The version of the pipeline, currently provided by the label of the adapter workflow'
-        ' around the analysis workflow.',
+        help="The version of the pipeline, currently provided by the label of the adapter workflow"
+        " around the analysis workflow.",
     )
-    parser.add_argument('--pipeline_type', required=True, help='Type of pipeline(SS2 or Optimus)')
-    parser.add_argument('--workspace_version', required=True, help='Workspace version value i.e. timestamp for workspace')
 
     args = parser.parse_args()
 
     analysis_protocol = AnalysisProtocol(
-        args.input_uuid,
         args.method,
-        args.pipeline_version,
+        args.input_uuid,
         args.pipeline_type,
+        args.pipeline_version,
         args.workspace_version
     )
 
     # Get the JSON content to be written
     analysis_protocol_json = analysis_protocol.get_json()
 
-    # Generate filename based on UUID and version
-    analysis_protocol_filename = f"{analysis_protocol_json['provenance']['document_id']}_{analysis_protocol_json['provenance']['submission_date']}.json"
+    # Generate filename based on entity id and workspace version
+    analysis_protocol_filename = f"{analysis_protocol.entity_id}_{analysis_protocol.workspace_version}.json"
 
     # Write analysis_protocol to file
-    print('Writing analysis_protocol.json to disk...')
+    print("Writing analysis_protocol.json to disk...")
     if not os.path.exists("analysis_protocol"):
         os.mkdir("analysis_protocol")
 
-    with open(f'analysis_protocol/{analysis_protocol_filename}', 'w') as f:
+    with open(f"analysis_protocol/{analysis_protocol_filename}", "w") as f:
         json.dump(analysis_protocol_json, f, indent=2, sort_keys=True)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
