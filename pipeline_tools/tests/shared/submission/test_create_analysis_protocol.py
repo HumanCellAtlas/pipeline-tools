1642e3db8b0d8d3f7084a75cff14705d22cd4007
cgold <cgold@asymmetrik.com>
2021-08-04 09:25:57 -0400

    adding test data for project level, updating analysis protocol to support project level

--- pipeline_tools/tests/shared/submission/test_create_analysis_protocol.py
+++ pipeline_tools/tests/shared/submission/test_create_analysis_protocol.py
@@ -12,6 +12,7 @@ def test_data():
         pipeline_type = "optimus"
         method = "https://dockstore.org/workflows/github.com/broadinstitute/warp/Optimus:Optimus_v4.2.3"
         pipeline_version = "optimus_v4.2.3"
+        project_level_pipeline_version = "optimus_post_processing_v1.0.0"
         workspace_version = "2021-05-24T12:00:00.000000Z"
 
     return Data
@@ -53,6 +54,35 @@ class TestCreateAnalysisProtocol(object):
         }
         assert analysis_protocol.get("provenance") == {
             "document_id": "f2cdb4e5-b439-5cdf-ac41-161ff39d5790",
+            "submission_date": "2021-05-24T12:00:00.000000Z",
+            "update_date": "2021-05-24T12:00:00.000000Z"
+        }
+
+
+class TestCreateAnalysisProtocolProjectLevel(object):
+    def test_build_analysis_protocol(self, test_data):
+        analysis_protocol = cap.test_build_analysis_protocol(
+            input_uuid=test_data.input_uuid,
+            pipeline_version=test_data.project_level_pipeline_version,
+            pipeline_type=test_data.pipeline_type,
+            workspace_version=test_data.workspace_version,
+            project_level=True
+        )
+
+        assert analysis_protocol.get("computational_method") == "optimus_post_processing_v1.0.0"
+        assert (
+            analysis_protocol.get("describedBy")
+            == "https://schema.humancellatlas.org/type/protocol/analysis/9.1.0/analysis_protocol"
+        )
+        assert analysis_protocol.get("schema_type") == "protocol"
+        assert analysis_protocol.get("type") == {
+            "text": "analysis; merge matrices"
+        }
+        assert analysis_protocol.get("protocol_core") == {
+            "protocol_id": "optimus_post_processing_v1.0.0"
+        }
+        assert analysis_protocol.get("provenance") == {
+            "document_id": "432a7422-59b5-5c46-8983-a7953f196781",
             "submission_date": "2021-05-24T12:00:00.000000Z",
             "update_date": "2021-05-24T12:00:00.000000Z"
         }
