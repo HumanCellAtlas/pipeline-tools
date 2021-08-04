f0f4687c4c46fb68ebf512dcae4c38c5c796a248
cgold <cgold@asymmetrik.com>
2021-08-04 14:00:34 -0400

    updating analaysis file script to generate project level files

--- pipeline_tools/tests/shared/submission/test_create_analysis_file.py
+++ pipeline_tools/tests/shared/submission/test_create_analysis_file.py
@@ -9,9 +9,13 @@ from pathlib import Path
 def test_data():
     class Data:
         input_uuid = 'heart_1k_test_v2_S1_L001'
-        metadata_json = 'pipeline_tools/tests/data/updated-data/staging/metdata/metadata.json'
+        project_level_input_uuid = '1fd499c5-f397-4bff-9af0-eb42c37d5fbe'
+        input_file = 'pipeline_tools/tests/data/updated-data/staging/metdata/metadata.json'
+        project_level_input_file = 'hca_adapter_testing/hca_adapter_test/hca_adapter_testing.loom'
         pipeline_type = 'optimus'
+        project_level_pipeline_type = 'OptimusPostProcessing'
         workspace_version = '2021-05-24T12:00:00.000000Z'
+        project_level = True
 
     return Data
 
@@ -30,7 +34,7 @@ class TestCreateAnalysisFile(object):
     def test_build_analysis_file(self, test_data):
         analysis_file = caf.test_build_analysis_file(
             input_uuid=test_data.input_uuid,
-            metadata_json=test_data.metadata_json,
+            input_file=test_data.input_file,
             pipeline_type=test_data.pipeline_type,
             workspace_version=test_data.workspace_version
         )
@@ -53,5 +57,40 @@ class TestCreateAnalysisFile(object):
                 }
             ],
             'file_name': 'heart_1k_test_v2_S1_L001.loom',
+            'format': 'loom'
+        }
+
+
+class TestCreateProjectLevelAnalysisFile(object):
+    def test_build_analysis_file(self, test_data):
+        analysis_file = caf.test_build_analysis_file(
+            input_uuid=test_data.project_level_input_uuid,
+            input_file=test_data.project_level_input_file,
+            pipeline_type=test_data.project_level_pipeline_type,
+            workspace_version=test_data.workspace_version,
+            project_level=test_data.project_level
+        )
+
+        assert (
+            analysis_file.get('describedBy')
+            == 'https://schema.humancellatlas.org/type/file/6.2.0/analysis_file'
+        )
+        assert analysis_file.get('schema_type') == 'file'
+        # TODO document_id being generated is not correct, fix this part of the unit test
+        # should be a13d652a-468a-541c-bb03-8fb521421fbd according to test data
+        assert analysis_file.get('provenance') == {
+            'document_id': 'a49db5bb-e25e-5c41-8fc6-c69edfb4281e',
+            'submission_date': '2021-05-24T12:00:00.000000Z',
+            'submitter_id': 'e67aaabe-93ea-564a-aa66-31bc0857b707'
+        }
+        assert analysis_file.get('file_core') == {
+            'content_description': [
+                {
+                    "ontology": "data:3917",
+                    "ontology_label": "Count Matrix",
+                    "text": "DCP/2-generated matrix"
+                }
+            ],
+            'file_name': 'hca_adapter_testing.loom',
             'format': 'loom'
         }
